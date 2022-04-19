12ae6e601a1eff85c95c26b4bfb2b9c9e96b0f51
Pavanreddy <103992287+pavansf@users.noreply.github.com>
2022-04-19 14:34:14 +0530

    Update streamlit_app.py
    added Second Menu items list

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🍌 Banana')
+streamlit.text('🥭 Mango')
+streamlit.text('🍇 Grapes')
